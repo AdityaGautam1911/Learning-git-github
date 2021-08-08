398e7e08625a6202b48d1fc3bb0a8d7dfd83cf19
AdityaGautam1911 <86793724+AdityaGautam1911@users.noreply.github.com>
2021-08-08 21:12:39 +0530

    list-AL-LL

---
+++ src/list/main.java
@@ -0,0 +1,13 @@
+//using the LinkedList that we created manually
+package list;
+import java.util.*;
+public class main {
+	public static void main(String[] args) {
+		constructingLinkedList myLL=new constructingLinkedList();
+		myLL.add(23);//calling add function from constructingLinkedList()
+		myLL.add(73);//calling add function from constructingLinkedList()
+		
+		myLL.print();//calling the print function from constructingLinkedList()class
+	}
+
+}
